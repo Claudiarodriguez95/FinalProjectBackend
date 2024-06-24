0105de77cc20efc467ea51dc01d02062e5a5e0d2
Claudiarodriguez95 <claudia_blue95@yahoo.com>
2024-06-23 20:03:59 -0500

    modified allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'https://www.dandrepairshop.com',
     'https://dandrepairshop.com',
-    'https://mernstackproject-zlq1.onrender.com'
+    
 ]
 
 module.exports = allowedOrigins
